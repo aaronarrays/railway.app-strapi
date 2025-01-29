cab7557e0f45fabae7087bb63cb4a0220190c398
Aaron Dev <aaronjosebricenomejia@gmail.com>
2025-01-29 10:23:17 -0500

    new content types

---
+++ src/api/wheel-collection/controllers/wheel-collection.ts
@@ -0,0 +1,7 @@
+/**
+ * wheel-collection controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::wheel-collection.wheel-collection');
---
+++ src/api/wheel-collection/services/wheel-collection.ts
@@ -0,0 +1,7 @@
+/**
+ * wheel-collection service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::wheel-collection.wheel-collection');
